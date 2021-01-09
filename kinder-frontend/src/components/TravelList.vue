0df40831da63f623676655302550d777adc10ad4
Michelle Hung <chialinghung74@gmail.com>
2021-01-09 21:35:33 +0800

    add travel photo list and add temp photo

---
+++ kinder-frontend/src/components/TravelList.vue
@@ -0,0 +1,63 @@
+<template>
+    <el-row>
+        <el-col :span="4" v-for="photo in 6" :key="photo" :offset="3" style="padding-bottom: 20px">
+            <el-card :body-style="{ padding: '0px' }">
+            <img :src="imagePath" class="image">
+            <div style="padding: 14px;">
+                <span>武嶺</span>
+                <div class="bottom clearfix">
+                <time class="time">{{ currentDate }}</time>
+                <el-button type="text" class="button">more</el-button>
+                </div>
+            </div>
+            </el-card>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+  name: 'TravelList',
+  props: {
+    
+  },
+  data() {
+    return {
+      currentDate: new Date(),
+      imagePath: require('@/assets/images/Wuling.jpeg')
+    };
+  }
+}
+</script>
+
+<style>
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+  
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+  
+  .clearfix:after {
+      clear: both
+  }
+</style>
--- kinder-frontend/src/views/Travel.vue
+++ kinder-frontend/src/views/Travel.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="travel">
     <h1>This is an travel page</h1>
+    <div class="list">
+      <travel-list></travel-list>
+    </div>
   </div>
+  
 </template>
+
+<script>
+import TravelList from '../components/TravelList.vue'
+
+export default {
+  name: 'Travel',
+  components: {
+    TravelList
+  }
+}
+</script>
