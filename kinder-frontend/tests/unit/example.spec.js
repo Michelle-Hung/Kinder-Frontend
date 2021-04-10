57b88a149b68f3d0c9dfa40548e4bb2c5cb5d4c2
Michelle Hung <chialinghung74@gmail.com>
2021-04-10 23:50:30 +0800

    add jest samples

--- kinder-frontend/tests/unit/example.spec.js
+++ kinder-frontend/tests/unit/example.spec.js
@@ -1,12 +1,62 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       props: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
+
+describe('demo', () => {
+  test('two plus two equal four', () => {
+    expect(2+2).toBe(4);
+  });
+
+  test('object assignment', () => {
+    const data = {one:1};
+    data['two'] = 2;
+    expect(data).toEqual({one:1, two:2});
+  });
+
+  test('adding positive numbers is not zero', () => {
+    for (let a = 1; a < 10; a++) {
+      for (let b = 1; b < 10; b++) {
+        expect(a + b).not.toBe(0);
+      }
+    }
+  });
+
+  test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+  
+  test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+  });
+
+  function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK');
+  }
+  
+  test('compiling android goes as expected', () => {
+    expect(() => compileAndroidCode()).toThrow();
+    expect(() => compileAndroidCode()).toThrow(Error);
+  
+    // You can also use the exact error message or a regexp
+    expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+    expect(() => compileAndroidCode()).toThrow(/JDK/);
+  });
 })
