4b343c0a2049844acd433c290d1757019c4ccbaa
Michelle Hung <chialinghung74@gmail.com>
2021-05-02 23:38:36 +0800

    add test sample

--- kinder-frontend/tests/unit/example.spec.js
+++ kinder-frontend/tests/unit/example.spec.js
@@ -39,12 +39,57 @@ describe('demo', () => {
   });
   
   test('zero', () => {
-    const z = 0;
+    const z = false;
     expect(z).not.toBeNull();
     expect(z).toBeDefined();
     expect(z).not.toBeUndefined();
     expect(z).not.toBeTruthy();
     expect(z).toBeFalsy();
+  });
+
+  test('two plus two', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+  
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+  });
+
+  test('两个浮点数字相加', () => {
+    const value = 0.1 + 0.2;
+    //expect(value).toBe(0.3);         //  这句会报错，因为浮点数有舍入误差
+    expect(value).toBeCloseTo(0.3); // 这句可以运行
+  });
+
+  test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+  });
+  
+  test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+  });
+
+  test('測試 email 格式是否正確', () => {
+    expect('gres@gmail.com').toMatch(
+      /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/
+    );
+  });
+
+  const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'milk',
+  ];
+  
+  test('the shopping list has milk on it', () => {
+    expect(shoppingList).toContain('milk');
+    expect(new Set(shoppingList)).toContain('milk');
   });
 
   function compileAndroidCode() {
