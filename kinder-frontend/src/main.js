89145aa257a5e54ff8bf8e9726b8d816105542e5
Michelle Hung <chialinghung74@gmail.com>
2021-05-14 00:49:10 +0800

    add axios and vue-axios

--- kinder-frontend/src/main.js
+++ kinder-frontend/src/main.js
@@ -3,8 +3,11 @@ import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
 
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
+app.use(VueAxios, axios)
 app.mount('#app')
