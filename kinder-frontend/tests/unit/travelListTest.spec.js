124a4cde3e75a0b58b9117a6eec06e115d9381c5
Michelle Hung <chialinghung74@gmail.com>
2021-05-24 23:32:30 +0800

    try unit test, but not work

--- kinder-frontend/tests/unit/travelListTest.spec.js
+++ kinder-frontend/tests/unit/travelListTest.spec.js
@@ -1,18 +1,13 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import TravelList from '../../src/components/TravelList.vue'
 
 describe('TravelList Test', () => {
     test('load travel list vue should call GetTravelData method', () => {
-        var wrapper = mount(TravelList)
-        wrapper.vm.loadTravelDatas = jest.fn()
-        expect(() => wrapper.vm.loadTravelDatas()).toBeCalledTimes(1);
-
-        /* try
         const loadTravelDatas = jest.fn()
         const wrapper = shallowMount(TravelList,{
           setup:{loadTravelDatas}
         });
         expect(loadTravelDatas).toHaveBeenCalled();
-        */
+        
       });
 })
