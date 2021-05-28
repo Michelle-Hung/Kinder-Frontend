d00a42550da62f0a54b977a8540e7451b6b6f953
Michelle Hung <michelle.hung@titansoft.com.sg>
2021-05-28 15:09:43 +0800

    try test

--- kinder-frontend/tests/unit/travelListTest.spec.js
+++ kinder-frontend/tests/unit/travelListTest.spec.js
@@ -1,13 +1,15 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import TravelList from '../../src/components/TravelList.vue'
+import getTravelDatas from '../../src/composables/GetTravelDatas'
+jest.mock('../../src/composables/GetTravelDatas',() => ({
+  load:  jest.fn()
+}))
 
 describe('TravelList Test', () => {
     test('load travel list vue should call GetTravelData method', () => {
-        const loadTravelDatas = jest.fn()
-        const wrapper = shallowMount(TravelList,{
-          setup:{loadTravelDatas}
-        });
-        expect(loadTravelDatas).toHaveBeenCalled();
+      
+        shallowMount(TravelList)
+        expect(getTravelDatas.load()).toHaveBeenCalled();
         
       });
 })
