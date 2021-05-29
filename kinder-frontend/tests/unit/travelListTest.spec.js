80a7a7292e2b4bf657aed8b02ae1e3a831c9b4cc
Michelle Hung <chialinghung74@gmail.com>
2021-05-30 00:39:33 +0800

    remove load, and test pass

--- kinder-frontend/tests/unit/travelListTest.spec.js
+++ kinder-frontend/tests/unit/travelListTest.spec.js
@@ -1,15 +1,12 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import TravelList from '../../src/components/TravelList.vue'
 import getTravelDatas from '../../src/composables/GetTravelDatas'
-jest.mock('../../src/composables/GetTravelDatas',() => ({
-  load:  jest.fn()
-}))
+jest.mock('../../src/composables/GetTravelDatas')
 
 describe('TravelList Test', () => {
     test('load travel list vue should call GetTravelData method', () => {
-      
         shallowMount(TravelList)
-        expect(getTravelDatas.load()).toHaveBeenCalled();
+        expect(getTravelDatas).toHaveBeenCalled();
         
       });
 })
