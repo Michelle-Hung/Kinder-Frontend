f7e837fb01a39d3781e50e4092ee381df5694c28
Michelle Hung <chialinghung74@gmail.com>
2021-05-30 02:05:53 +0800

    add input box and format

--- kinder-frontend/src/components/TravelList.vue
+++ kinder-frontend/src/components/TravelList.vue
@@ -1,18 +1,25 @@
 <template>
-    <el-row>
-        <el-col :span="4" v-for="travel in travelDatas" :key="travel.id" :offset="3" style="padding-bottom: 20px">
-            <el-card :body-style="{ padding: '0px' }">
-            <img :src="require('@/assets/images/Wuling.jpeg')" class="image">
-            <div style="padding: 14px;">
-                <span>{{ travel.attraction }}</span>
-                <div class="bottom clearfix">
-                <time class="time" format="YYYY/MM/DD">{{ travel.startDate }}</time>
-                <el-button type="text" class="button">more</el-button>
-                </div>
-            </div>
-            </el-card>
-        </el-col>
-    </el-row>
+  <el-input placeholder="Searching..." v-model="input" clearable size="small"> </el-input>
+  <el-row>
+    <el-col
+      :span="4"
+      v-for="travel in travelDatas"
+      :key="travel.id"
+      :offset="3"
+      style="padding-bottom: 20px"
+    >
+      <el-card :body-style="{ padding: '0px' }">
+        <img :src="require('@/assets/images/Wuling.jpeg')" class="image" />
+        <div style="padding: 14px">
+          <span>{{ travel.attraction }}</span>
+          <div class="bottom clearfix">
+            <time class="time" format="YYYY/MM/DD">{{ travel.startDate }}</time>
+            <el-button type="text" class="button">more</el-button>
+          </div>
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -22,42 +29,45 @@ export default {
   setup() {
     //const imagePath = require('@/assets/images/Wuling.jpeg')
 
-  
-    const { travelDatas } = getTravelDatas()
+    const { travelDatas } = getTravelDatas();
 
-    return { travelDatas }
-  }
-}
+    return { travelDatas };
+  },
+};
 </script>
 
 <style>
-  .time {
-    font-size: 13px;
-    color: #999;
-  }
-  
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
+.time {
+  font-size: 13px;
+  color: #999;
+}
 
-  .button {
-    padding: 0;
-    float: right;
-  }
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
 
-  .image {
-    width: 100%;
-    display: block;
-  }
+.button {
+  padding: 0;
+  float: right;
+}
 
-  .clearfix:before,
-  .clearfix:after {
-      display: table;
-      content: "";
-  }
-  
-  .clearfix:after {
-      clear: both
-  }
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
+
+.el-input {
+  width: 180px;
+}
 </style>
