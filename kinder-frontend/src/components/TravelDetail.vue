3ae7d530bf589709dc79bcf3898e00635a1d5b08
Michelle Hung <chialinghung74@gmail.com>
2021-06-01 01:01:09 +0800

    add travelDetail page

---
+++ kinder-frontend/src/components/TravelDetail.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1> Travel Detail </h1>
+    <p> Travel id is {{ id }}</p>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+import { useRoute } from "vue-router";
+export default {
+    setup() {
+        const route = useRoute()
+        const id = ref(0)
+        id.value = route.params.id
+
+        return { id }
+    },
+}
+</script>
