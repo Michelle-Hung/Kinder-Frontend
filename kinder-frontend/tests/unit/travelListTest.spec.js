a38a80f4f75016a59abac43c8f9a0e7016ea2ac5
Michelle Hung <chialinghung74@gmail.com>
2021-06-01 01:07:54 +0800

    add test "click more will call GoToTravelDetailPage" but not work
    it can't be mounted, error show empty wrapper

--- kinder-frontend/tests/unit/travelListTest.spec.js
+++ kinder-frontend/tests/unit/travelListTest.spec.js
@@ -1,12 +1,23 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import TravelList from '../../src/components/TravelList.vue'
 import getTravelDatas from '../../src/composables/GetTravelDatas'
+
 jest.mock('../../src/composables/GetTravelDatas')
 
 describe('TravelList Test', () => {
     test('load travel list vue should call GetTravelData method', () => {
+        //TODO : fix can't mount DOM when "const {travelDatas} = getTravelDatas "
         shallowMount(TravelList)
         expect(getTravelDatas).toHaveBeenCalled();
         
-      });
+      }),
+      test('click more will call GoToTravelDetailPage', () => {
+        //TODO: fix can't mount element ui plus DOM, it only has <el-row></el-row>
+        const wrapper = shallowMount(TravelList);
+        wrapper.vm.GoToTravelDetailPage = jest.fn()
+        console.log(wrapper.html())
+        wrapper.find('.el-button').trigger('click')
+        //wrapper.findComponent({ ref: 'MoreTravelDataButton' }).trigger('click')
+        expect(wrapper.vm.GoToTravelDetailPage).toHaveBeenCalled()
+      })
 })
