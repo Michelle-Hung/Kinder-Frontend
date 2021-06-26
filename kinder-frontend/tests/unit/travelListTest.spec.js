928c94b54c3a649306598de5adf91aa884018412
Michelle Hung <chialinghung74@gmail.com>
2021-06-27 01:00:55 +0800

    try mock return value with other way, and fix vue-router warning
    work fine

--- kinder-frontend/tests/unit/travelListTest.spec.js
+++ kinder-frontend/tests/unit/travelListTest.spec.js
@@ -2,22 +2,31 @@ import { mount, shallowMount } from '@vue/test-utils'
 import TravelList from '../../src/components/TravelList.vue'
 import { getTravelDatas } from '../../src/composables/GetTravelDatas'
 import ElementPlus from 'element-plus'
+import { useRouter } from 'vue-router'
 
-jest.mock('../../src/composables/GetTravelDatas')
-getTravelDatas.mockReturnValue([]);
-
-const wrapper = shallowMount(TravelList, {
-  global: {
-      plugins: [ElementPlus]
-  }
-})
+jest.mock('../../src/composables/GetTravelDatas');
+jest.mock('vue-router')
 
 describe('TravelList Test', () => {
     test('load travel list vue should call GetTravelData method', () => {
+      require('../../src/composables/GetTravelDatas').getTravelDatas.mockReturnValue([])
+      require('vue-router').useRouter.mockReturnValue({})
+      const wrapper = shallowMount(TravelList, {
+        global: {
+            plugins: [ElementPlus]
+        }
+      });
       expect(getTravelDatas).toHaveBeenCalled();
 
     });
     test('click more will call GoToTravelDetailPage', () => {
+      require('../../src/composables/GetTravelDatas').getTravelDatas.mockReturnValue([])
+      require('vue-router').useRouter.mockReturnValue({})
+      const wrapper = shallowMount(TravelList, {
+        global: {
+            plugins: [ElementPlus]
+        }
+      });
       wrapper.vm.GoToTravelDetailPage = jest.fn()
       console.log(wrapper.html())
       wrapper.find('.el-button').trigger('click')
