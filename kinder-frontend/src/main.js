cdb81ed8fa17e6a24dbd1224f38f55448c85a2f7
Michelle Hung <chialinghung74@gmail.com>
2021-08-07 17:14:45 +0800

    change order

--- kinder-frontend/src/main.js
+++ kinder-frontend/src/main.js
@@ -3,8 +3,8 @@ import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
-import VueAxios from 'vue-axios'
 import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 const app = createApp(App)
 app.use(ElementPlus)
