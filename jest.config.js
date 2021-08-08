a4f5c3911b7b99fc7ce3840048c8530c0c117c93
Michelle Hung <chialinghung74@gmail.com>
2021-08-09 01:29:50 +0800

    add testMatch in jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   transform: {
     '^.+\\.vue$': 'vue-jest'
-  }
+  },
+  testMatch: ["**/tests/unit/**/*.spec.[jt]s?(x)", "**/__tests__/*.[jt]s?(x)"]
 }
