74ff310f230848ed55569a094483e85a2f1f4f38
Michelle Hung <chialinghung74@gmail.com>
2021-08-22 01:06:25 +0800

    fix test

--- tests/unit/travelListTest.spec.js
+++ tests/unit/travelListTest.spec.js
@@ -1,6 +1,6 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import TravelList from '../../src/components/TravelList.vue'
-import { getTravelDatas } from '../../src/composables/GetTravelDatas'
+import getTravelDatas from '../../src/composables/GetTravelDatas'
 import ElementPlus from 'element-plus'
 import { useRouter } from 'vue-router'
 
@@ -9,8 +9,8 @@ jest.mock('vue-router')
 
 describe('TravelList Test', () => {
     test('load travel list vue should call GetTravelData method', () => {
-      require('../../src/composables/GetTravelDatas').getTravelDatas.mockReturnValue([])
-      require('vue-router').useRouter.mockReturnValue({})
+      getTravelDatas.mockReturnValue([]);
+      useRouter.mockReturnValue({});
       const wrapper = shallowMount(TravelList, {
         global: {
             plugins: [ElementPlus]
