355526343b36d5ff287c0265d322e95a1ccfdbda
Michelle Hung <chialinghung74@gmail.com>
2021-08-21 18:50:44 +0800

    remove unneccessary property

--- src/composables/GetTravelDatas.js
+++ src/composables/GetTravelDatas.js
@@ -5,11 +5,7 @@ export function getTravelDatas() {
     const travelDatas = ref([])
         axios.get('https://localhost:5001/MyTravel/GetTravelData')
         .then((response) => {
-        if (!response.data.success) {
-            alert(response.data.errorMessage)
-        }else{
             travelDatas.value = response.data.travelDetail
-        }
         }).catch((error) => {
         alert('Unexpected Error: ', error.message)
         console.log(error)
