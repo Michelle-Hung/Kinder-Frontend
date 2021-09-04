afd33ef3578b91b2b5f44a5b0e51ae6594a149b9
Michelle Hung <chialinghung74@gmail.com>
2021-09-04 22:50:07 +0800

    add a cypress test for practicing

--- cypress/integration/example/myFirstTest_spec.js
+++ cypress/integration/example/myFirstTest_spec.js
@@ -2,4 +2,10 @@ describe('My First Test', () => {
     it('visit github should success', () =>{
         cy.visit('https://github.com');
     });
+    it('udemy class test', () => {
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+        cy.get('.search-keyword').type('ca') //type: input 'ca'
+        cy.wait(2000)
+        cy.get('.product:visible').should('have.length',4)
+    })
 })
