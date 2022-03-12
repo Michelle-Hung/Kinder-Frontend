cb4d1059bfc4c7ea321d568ff95ecc7850b59e45
Michelle Hung <chialinghung74@gmail.com>
2022-03-13 00:40:06 +0800

    adjust css

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,43 +1,46 @@
 <template>
   <v-container>
     <v-col cols="12" sm="6">
-      <v-card class="mx-auto" max-width="600">
+      <v-card class="mx-auto" color="#F5F5F5">
         <v-card-avatar>
           <v-avatar>
             <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
           </v-avatar>
           John
         </v-card-avatar>
-        <v-row v-for="(message, index) in messageContent" :key="index">
-          <v-col cols="12" sm="6">
-            <v-card-avatar>
-              <v-avatar size="small">
-                <img
-                  src="https://cdn.vuetifyjs.com/images/john.jpg"
-                  alt="John"
-                />
-              </v-avatar>
-            </v-card-avatar>
-            <v-card>
-              <v-card-text>
-                {{ message }}
-              </v-card-text>
-            </v-card>
-          </v-col>
+        <v-row
+          v-for="(message, index) in messageContent"
+          :key="index"
+          class="mt-5"
+        >
+          <v-card-avatar>
+            <v-avatar size="small">
+              <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+            </v-avatar>
+          </v-card-avatar>
+          <v-card>
+            <v-card-text>
+              {{ message }}
+            </v-card-text>
+          </v-card>
         </v-row>
         <v-card-actions>
-          <v-row style="padding: 0.5rem">
+          <v-row class="mt-5 px-2">
             <v-text-field
               v-model="newMessage"
+              variant="filled"
               label="Message"
               placeholder="Type a message here"
               type="text"
-              append-icon="mdi-send"
+              append-icon="mdi:mdi-send"
               clearable
             ></v-text-field>
-            <v-btn icon @click="sendMessage">
-              <v-icon color="light-blue lighten-3">mdi-send</v-icon>
-            </v-btn>
+            <v-btn
+              icon="mdi:mdi-send"
+              color="light-blue lighten-3"
+              @click="sendMessage"
+              variant="plain"
+            />
           </v-row>
         </v-card-actions>
       </v-card>
