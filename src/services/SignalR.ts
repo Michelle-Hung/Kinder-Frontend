4e6f7ccfdce8280396e4d9212507cea6b3b893fd
Michelle Hung <chialinghung74@gmail.com>
2022-03-13 02:01:14 +0800

    using reactive

--- src/services/SignalR.ts
+++ src/services/SignalR.ts
@@ -1,37 +1,28 @@
-import { HubConnectionState, HubConnectionBuilder, HubConnection } from "@microsoft/signalr";
-import { ref } from "vue";
+import { HubConnectionState, HubConnectionBuilder } from "@microsoft/signalr";
+import { reactive } from "vue";
 const connection = new HubConnectionBuilder()
-        .withUrl("https://localhost:5001/ChatHub")
-        .withAutomaticReconnect()
-        .build();
+  .withUrl("https://localhost:5001/ChatHub")
+  .withAutomaticReconnect()
+  .build();
 export function signalrInit() {
-    // const connection = new HubConnectionBuilder()
-    //     .withUrl("https://localhost:5001/ChatHub")
-    //     .withAutomaticReconnect()
-    //     .build();
-    const messageContent = ref<Array<string>>([]);
-    connection
-      .start()
-      .then(() => {
-        if (connection.state !== HubConnectionState.Connected) {
-          // this.connect().finally(() => {
-          // this.listen();
-          // return;});
-          console.log(connection.state);
-        }
-        connection.on("ReceiveMessage", (res: string) => {
-          messageContent.value.push(res);
-          console.log(
-            `receive message :: ${JSON.stringify(messageContent.value)}`
-          );
-        });
-      })
-      .catch((err) => {
-        console.log(`Connection Error ${err}`);
+  const messageContent = reactive<Array<string>>([]);
+  connection
+    .start()
+    .then(() => {
+      if (connection.state !== HubConnectionState.Connected) {
+        console.log(connection.state);
+      }
+      connection.on("ReceiveMessage", (res: string) => {
+        messageContent.push(res);
+        console.log(`receive message :: ${JSON.stringify(messageContent)}`);
       });
-    connection.onclose(() => {
-      console.log("Connection Destroy");
+    })
+    .catch((err) => {
+      console.log(`Connection Error ${err}`);
     });
-    
-    return { messageContent, connection }
+  connection.onclose(() => {
+    console.log("Connection Destroy");
+  });
+
+  return { messageContent, connection };
 }
