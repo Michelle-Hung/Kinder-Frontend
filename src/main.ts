4bae31737e83599467ec61f2bfec8388637c3b9f
Michelle Hung <chialinghung74@gmail.com>
2022-08-25 17:02:09 +0800

    install pinia and fix npm run serve

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import { createPinia } from 'pinia'
 
 loadFonts();
 
-createApp(App).use(router).use(store).use(vuetify).mount("#app");
+createApp(App).use(createPinia()).use(router).use(store).use(vuetify).mount("#app");
