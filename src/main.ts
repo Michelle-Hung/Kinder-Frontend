f25d99318029445b7e4fa94e88fded821445d1c6
Michelle Hung <chialinghung74@gmail.com>
2022-08-25 17:03:34 +0800

    fix eslint

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,13 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
-import { createPinia } from 'pinia'
+import { createPinia } from "pinia";
 
 loadFonts();
 
-createApp(App).use(createPinia()).use(router).use(store).use(vuetify).mount("#app");
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount("#app");
