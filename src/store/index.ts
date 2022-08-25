6b4df2ac1caa9f1a1986189b9c98e8e8740acd68
Michelle Hung <chialinghung74@gmail.com>
2022-08-25 18:48:00 +0800

    comment vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,23 @@
 import { createStore } from "vuex";
-import { UserInfo } from "@/models/UserInfo";
 
 export default createStore({
-  state: {
-    userInfo: {} as UserInfo,
-  },
-  getters: {
-    userInfo(state) {
-      return state.userInfo;
-    },
-  },
-  mutations: {
-    setUserInfo(state, userInfo) {
-      state.userInfo = userInfo;
-    },
-  },
-  actions: {
-    setUserInfo({ commit }, userInfo) {
-      commit("setUserInfo", userInfo);
-    },
-  },
-  modules: {},
+  // state: {
+  //   userInfo: {} as UserInfo,
+  // },
+  // getters: {
+  //   userInfo(state) {
+  //     return state.userInfo;
+  //   },
+  // },
+  // mutations: {
+  //   setUserInfo(state, userInfo) {
+  //     state.userInfo = userInfo;
+  //   },
+  // },
+  // actions: {
+  //   setUserInfo({ commit }, userInfo) {
+  //     commit("setUserInfo", userInfo);
+  //   },
+  // },
+  // modules: {},
 });
